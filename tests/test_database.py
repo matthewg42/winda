ea93b70efd95edb58eebaf2609d05154ef52344b
Matthew Gates <matthewg42@gmail.com>
2016-01-13 15:43:01 +0000

    make sure db files doesn't pre-exist

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,9 +5,13 @@ from wind.database import Database
 
 class TestDatabase(unittest.TestCase):
     def test_create_database_creation(self):
-        tmpfile = tempfile.NamedTemporaryFile(suffix='sqlite3')
+        tmpfile = tempfile.NamedTemporaryFile(suffix='.sqlite3')
+        tmpfile.close()
+        self.assertTrue(not os.path.exists(tmpfile.name))
         d = Database(tmpfile.name)
+        self.assertTrue(os.path.exists(tmpfile.name))
         self.assertTrue(d.schema_exists())
-
-    
+        # This will close the database file
+        del(d)
+        os.remove(tmpfile.name)
         

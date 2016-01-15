d0dbb3df8cc229df7984abecdd749ad6e81370ff
Matthew Gates <matthewg42@gmail.com>
2016-01-15 15:58:20 +0000

    add test for accuracy of event data

--- tests/test_add_functions.py
+++ tests/test_add_functions.py
@@ -64,6 +64,42 @@ BB,12-01-2016,19:34:17,1,2,SW,0.50,4.72
         self.assertEqual(data[1][7], 4.72)
         self.assertEqual(data[1][8], 1) # not record 0 will not be flagged as processed
 
+    def test_process_data(self):
+        c = self._db._conn.cursor()
+        c.execute("""
+                  SELECT    ref, file_id, event_start, event_end, event_duration, 
+                            anemometer_hz_1, anemometer_hz_2, irradiance_v, 
+                            windspeed_ms_1, windspeed_ms_2, wind_direction,
+                            irradiance_wm2
+                  FROM      event
+                  """)
+        events = c.fetchall()
+        self.assertEqual(len(events), 4)
+        # Check the first event, field by field
+        self.assertEqual(events[0][0], 'BB')
+        self.assertEqual(events[0][2], '2016-01-12 19:34:10')
+        self.assertEqual(events[0][3], '2016-01-12 19:34:11')
+        self.assertEqual(events[0][4], 1.0)    # duration
+        self.assertEqual(events[0][5], 1.0)    # anemometer_hz_1
+        self.assertEqual(events[0][6], 2.0)    # anemometer_hz_2
+        self.assertEqual(events[0][7], 0.2)    # irradiance_v
+        self.assertEqual(events[0][8], 1.42)   # windspeed_ms_1
+        self.assertEqual(events[0][9], 2.84)   # windspeed_ms_1
+        self.assertEqual(events[0][10], 'N')  # irradiance_wm2
+        self.assertEqual(events[0][11], 0.2)  # irradiance_wm2
+        # Check the last record, field by field
+        self.assertEqual(events[3][0], 'BB')
+        self.assertEqual(events[3][2], '2016-01-12 19:34:16')
+        self.assertEqual(events[3][3], '2016-01-12 19:34:17')
+        self.assertEqual(events[3][4], 1.0)    # duration
+        self.assertEqual(events[3][5], 1.0)    # anemometer_hz_1
+        self.assertEqual(events[3][6], 2.0)    # anemometer_hz_2
+        self.assertEqual(events[3][7], 0.5)    # irradiance_v
+        self.assertEqual(events[3][8], 1.42)   # windspeed_ms_1
+        self.assertEqual(events[3][9], 2.84)   # windspeed_ms_1
+        self.assertEqual(events[3][10], 'SW')  # irradiance_wm2
+        self.assertEqual(events[3][11], 0.5)  # irradiance_wm2
+
     def get_input_file_id(self):
         c = self._db._conn.cursor()
         c.execute("""SELECT id FROM input_file WHERE path = ?""", (self._testfile.name,))

9b892d3632c94a99c8eb541cafd068c55b9b4446
Matthew Gates <matthewg42@gmail.com>
2016-01-17 01:14:27 +0000

    create Filter class and first test

---
+++ tests/test_filter_functions.py
@@ -0,0 +1,48 @@
+import unittest
+import os
+import tempfile
+from wind.database import Database
+from wind.filter import Filter
+
+class TestFilterFunctions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Create an empty database
+        tmpfile = tempfile.NamedTemporaryFile(suffix='.sqlite3')
+        cls._path = tmpfile.name
+        del(tmpfile)
+        cls._db = Database(cls._path)
+        # Create a file with test data in it
+        cls._testfile = tempfile.NamedTemporaryFile(suffix='.csv', delete=False)
+        cls._testfile.write("""Ref, Date, Time, Wind 1, Wind 2, Direction, Irradiance Wm-2, Batt V
+BB,12-01-2016,19:34:10,1,2,W,0.10,4.72
+BB,12-01-2016,19:34:11,1,2,N,0.20,4.72
+BB,12-01-2016,19:34:15,1,2,E,0.30,4.70
+BB,12-01-2016,19:34:16,1,2,S,0.40,4.72
+BB,12-01-2016,19:34:17,1,2,SW,0.50,4.72
+""")
+        cls._testfile.close()
+        # Add the test data to the test database
+        cls._db.add([cls._testfile.name])
+        c = cls._db._conn.cursor()
+
+    @classmethod
+    def tearDownClass(cls):
+        del(cls._db)
+        os.remove(cls._path)
+        os.remove(cls._testfile.name)
+
+    def test_no_filters(self):
+        c = self._db._conn.cursor()
+        filt = Filter(file_filter=None, date_filter=None, from_filter=None, to_filter=None)
+        events = filt.select_events(c)
+        print('ALL (%d)' % len(events))
+        self.assertEqual(len(events), 4)
+
+    def test_no_filters(self):
+        c = self._db._conn.cursor()
+        filt = Filter(file_filter=None, date_filter=None, from_filter=None, to_filter=None)
+        events = filt.select_events(c)
+        print('COUNT (%d)' % len(events))
+        self.assertEqual(len(events), 4)
+
---
+++ wind/filter.py
@@ -0,0 +1,124 @@
+from wind.database import result_as_dict_array
+
+""" Class which describes data filters """
+class Filter:
+    def __init__(self, file_filter=None, date_filter=None, from_filter=None, to_filter=None):
+        """
+        Create a Filter object.
+
+        file_filter should be a path in the input_file table
+        date_filter is a datetime.date object
+        from_filter is a datetime.datetime object
+        to_filter is a datetime.datetime object
+        """
+        self.file_filter = file_filter
+        self.date_filter = date_filter
+        self.from_filter = from_filter
+        self.to_filter = to_filter
+
+    def get_all(self):
+        return self.file_filter is None and self.date_filter is None and self.from_filter is None and self.to_filter is None
+
+    def select_events(self, cursor):
+        if self.get_all():
+            cursor.execute("""SELECT * FROM event""")
+            return result_as_dict_array(cursor)
+        cursor.execute("""CREATE TEMP TABLE tmp_event_rids (rid INT)""")
+        cursor.execute("""INSERT INTO tmp_event_rids SELECT rowid FROM event""")
+        log.debug('Events selected before filtering: %d' % self.count_selected_events(cursor))
+        # For each filter which is defined remove the set of records not matching the filter
+        if self.file_filter is not None:
+            cursor.execute("""
+                           DELETE FROM tmp_event_rids
+                           WHERE NOT EXISTS (
+                               SELECT        1
+                               FROM          event e,
+                                             input_file i
+                               WHERE         rid = e.rowid
+                               AND           i.id = e.file_id
+                               AND           i.path = ?
+                           )
+                           """, (self.file_filter,))
+        log.debug('Events selected after file_filter(s): %d' % (
+            file_filter, self.count_selected_events(cursor)))
+
+        if self.date_filter is not None:
+            cursor.execute("""
+                           DELETE FROM tmp_event_rids
+                           WHERE NOT EXISTS (
+                               SELECT        1
+                               FROM          event e
+                               WHERE         rid = e.rowid
+                               AND           event_end LIKE ?
+                           )
+                           """, (self.date_filter.strftime('%Y-%m-%d%%'),))
+        log.debug('Events selected after date_filter(%s): %d' % (
+                    str(date_filter), self.count_selected_events(cursor)))
+
+        if self.from_filter is not None and self.to_filter is not None:
+            cursor.execute("""
+                           DELETE FROM tmp_event_rids
+                           WHERE NOT EXISTS (
+                               SELECT        1
+                               FROM          event e
+                               WHERE         rid = e.rowid
+                               AND           event_end >= ?
+                               AND           event_end <= ?
+                           )
+                           """, (self.from_filter.strftime('%Y-%m-%d %T'),
+                                 self.from_filter.strftime('%Y-%m-%d %T')))
+        elif self.from_filter is not None and self.to_filter is None:
+            cursor.execute("""
+                           DELETE FROM tmp_event_rids
+                           WHERE NOT EXISTS (
+                               SELECT        1
+                               FROM          event
+                               WHERE         rid = e.rowid
+                               AND           event_end >= ?
+                           )
+                           """, (self.from_filter.strftime('%Y-%m-%d %T'),))
+        elif self.from_filter is not None and self.to_filter is None:
+            cursor.execute("""
+                           DELETE FROM tmp_event_rids
+                           WHERE NOT EXISTS (
+                               SELECT        1
+                               FROM          event
+                               WHERE         rid = e.rowid
+                               AND           event_end >= ?
+                           )
+                           """, (self.from_filter.strftime('%Y-%m-%d %T'),))
+        log.debug('Events selected after from_filter(%s) to_filter(%s): %d' % (
+                    str(from_filter), str(to_filter), self.count_selected_events(cursor)))
+
+        cursor.execute("""
+                       SELECT        *
+                       FROM          event e
+                       WHERE         EXISTS (
+                           SELECT       1
+                           FROM         tmp_event_rids t
+                           WHERE        e.rowid = t.rid
+                       )
+                       """)
+        return result_as_dict_array(cursor)
+            
+    def count_selected_events(self, cursor):
+        try:
+            cursor.execute("""
+                           SELECT       1
+                           FROM         event e,
+                                        tmp_event_rids t
+                           WHERE        e.rowid = t.rid
+                           """)
+            return len(cursor.fetchall())
+        except Exception as e:
+            log.warning('Filter.count_selected_events() exception: %s / %s' % (type(e), e))
+            return 0
+
+    def select_raw_data(self, cursor):
+        if self.get_all():
+            cursor.execute("""SELECT * FROM raw_data""")
+            return cursor.fetchall()
+        return []
+
+
+

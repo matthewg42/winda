dd39ea9ff1c6e71ac86454d5d0fbeebf5e982308
Matthew Gates <matthewg42@gmail.com>
2016-01-17 16:04:50 +0000

    improve build process

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-alias 7z='/c/Progra~1/7-zip/7z'
 rm -rf dist build win32/winda.zip
 python setup.py py2exe
 rm -rf build
-7z a win32/winda.zip dist/*
-
+cd dist
+/c/Progra~1/7-zip/7z a ../win32/winda.zip *
